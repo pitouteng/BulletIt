8c332e0986cc31bf3631e5653b04f69f4af07a42
Jason Moynihan <jasonmoynihan@Jasons-MBP-3.fios-router.home>
2020-11-18 18:34:06 -0500

    First Look at Monthly View

--- app/src/main/java/com/example/bulletit/MainActivity.java
+++ app/src/main/java/com/example/bulletit/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.bulletit;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+        import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.widget.Button dateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateBtn = findViewById(R.id.DateButton);
+
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToMonthlyViewActivity();
+            }
+        });
+    }
+
+    private void moveToMonthlyViewActivity(){
+        android.content.Intent intent = new android.content.Intent(MainActivity.this, MonthlyViewActivity.class);
+        startActivity(intent);
     }
 }
