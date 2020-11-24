5f03ca2927ac5148e4f3f316d2047e9955bb906a
Pitou Teng <33436707+pitouteng@users.noreply.github.com>
2020-11-23 22:56:11 -0500

    Implemented add todo item

---
+++ app/src/main/java/com/example/bulletit/AddTodoitem.java
@@ -0,0 +1,65 @@
+package com.example.bulletit;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class AddTodoitem  extends AppCompatDialogFragment {
+    private EditText editTextAddTodoItem;
+    private AddTodoItemListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.add_todo_popup, null);
+
+        builder.setView(view)
+                .setTitle("Todo Item")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String todoItem = editTextAddTodoItem.getText().toString();
+                        listener.applyTodoItemn(todoItem);
+                    }
+                });
+
+        editTextAddTodoItem = view.findViewById(R.id.edit_text_todo_item);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (AddTodoItemListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement AddTodoItemDialog");
+        }
+
+
+    }
+
+    public interface AddTodoItemListener{
+        void applyTodoItemn(String todoItem);
+    }
+}
--- app/src/main/java/com/example/bulletit/MainActivity.java
+++ app/src/main/java/com/example/bulletit/MainActivity.java
@@ -2,28 +2,61 @@ package com.example.bulletit;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.app.Activity;
+        import android.content.Context;
         import android.os.Bundle;
+        import android.text.InputType;
+        import android.util.Log;
+        import android.view.Gravity;
+        import android.view.KeyEvent;
+        import android.view.LayoutInflater;
         import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.inputmethod.EditorInfo;
+        import android.view.inputmethod.InputMethodManager;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.LinearLayout;
+        import android.widget.PopupWindow;
+        import android.widget.TextView;
+
+        import org.w3c.dom.Text;
 
         import java.text.DateFormat;
         import java.text.SimpleDateFormat;
         import java.util.Calendar;
         import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddTodoitem.AddTodoItemListener {
 
     private android.widget.Button dateBtn;
+    private Object MainActivity;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         dateBtn = findViewById(R.id.DateButton);
         Date currentTime = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("E, MMM dd");
+        DateFormat dateFormat = new SimpleDateFormat("E MMM dd");
         String dateString = dateFormat.format(currentTime);
+
+        final TextView todoView = (TextView) findViewById(R.id.todo);
+        System.out.println(this.toString());
+//        final TextView todoItem = new TextView(this.);
+
+        todoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTodoItemDialog();
+            }
+        });
+
+//        final EditText zoop = (EditText) findViewById(R.id.zoop);
+//        zoop.setun
 
 
         dateBtn.setText(dateString);
@@ -36,8 +69,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public void addTodoItemDialog(){
+        AddTodoitem addTodoitem = new AddTodoitem();
+        addTodoitem.show(getSupportFragmentManager(), "Add Todo Item");
+    }
+
     private void moveToMonthlyViewActivity(){
         android.content.Intent intent = new android.content.Intent(MainActivity.this, MonthlyViewActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void applyTodoItemn(String todoItem) {
+        TextView newTodoItem = new TextView(this);
+        newTodoItem.setText(todoItem);
+        newTodoItem.setGravity(Gravity.CENTER_VERTICAL);
+
+        ViewGroup.LayoutParams params = (ViewGroup.LayoutParams) newTodoItem.getLayoutParams();
+//        params.height = getResources().getDimensionPixelSize(R.dimen.todo_text_view_height);
+        newTodoItem.setHeight(getResources().getDimensionPixelSize(R.dimen.todo_text_view_height));
+        newTodoItem.setPadding(25, 0, 0 , 0);
+//        newTodoItem.setLayoutParams(params);
+
+        LinearLayout todoBox = findViewById(R.id.todoBox);
+        todoBox.addView(newTodoItem);
     }
 }
